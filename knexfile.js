0fa5455c92e580e2cc42395132598c0fc07838c0
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-21 10:13:32 -0700

    Solved cascading problem and changed the package json for heroku

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: { filename: './database/kickstarter.db3' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
@@ -9,4 +14,18 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+
+
+  production: {
+    client: 'pg', // Install this package
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
+
