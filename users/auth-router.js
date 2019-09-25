a9c6ad0d03de5810010117a1effb62c166fca00f
Timothy McGowen <mcgowen17@yahoo.com>
2019-09-24 23:16:13 -0700

    Debugging stuff

--- users/auth-router.js
+++ users/auth-router.js
@@ -93,10 +93,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = generateToken(user)
-
-
-
-                res.status(200).json(token)
+                user.token = token;
+                res.status(200).json(user)
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' })
             }
